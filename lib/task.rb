dd6ed136c1d1cc95cf51bf4d58e94997ee9812da
Matt Wilkin and Sky Rousse <sky@>
2016-07-11 15:50:21 -0700

    add ORDER BY command into the DB exec in the task.rb file

--- lib/task.rb
+++ lib/task.rb
@@ -8,7 +8,7 @@ class Task
   end
 
   define_singleton_method(:all) do
-    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    returned_tasks = DB.exec("SELECT * FROM tasks ORDER BY completion_date ASC;")
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
