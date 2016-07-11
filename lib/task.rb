0557be40fec6cea421bd70130e0f5f8994b92e29
Matt Wilkin and Sky Rousse <matt@>
2016-07-11 15:28:29 -0700

    add new spec for completetion date

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,10 @@
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :completion_date)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @completion_date = attributes.fetch(:completion_date)
   end
 
   define_singleton_method(:all) do
@@ -12,13 +13,14 @@ class Task
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch('list_id').to_i()
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      completion_date = task.fetch("completion_date")
+      tasks.push(Task.new({:description => description, :list_id => list_id, :completion_date => completion_date}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, completion_date) VALUES ('#{@description}', #{@list_id}, '#{@completion_date}');")
   end
 
   define_method(:==) do |another_task|
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -12,7 +12,7 @@ describe(Task) do
 
   describe('#save') do
     it('adds a task to the array of saved tasks') do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :completion_date => "1998-12-31"})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -20,22 +20,29 @@ describe(Task) do
 
   describe('#description') do
     it('lets you give it a description') do
-    test_task = Task.new({:description => "learn SQL", :list_id => 1})
+    test_task = Task.new({:description => "learn SQL", :list_id => 1, :completion_date => "1998-12-31"})
     expect(test_task.description()).to(eq('learn SQL'))
     end
   end
 
   describe('#list_id') do
     it('lets you read the list ID out') do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :completion_date => "1998-12-31"})
       expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe('#completion_date') do
+    it('lets you give the task a due date') do
+      test_task = Task.new({:description => "learn SQL",:list_id => 1, :completion_date => "1983-07-08"})
+      expect(test_task.completion_date()).to(eq("1983-07-08"))
     end
   end
 
   describe('#==') do
     it('is the same task if it has the same description and list ID') do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :completion_date => "1998-12-31"})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :completion_date => "1998-12-31"})
       expect(task1).to(eq(task2))
     end
   end
