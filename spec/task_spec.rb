42509c28c597afd6b270bd988b59dd3e6d680c04
Matt Wilkin and Sky Rousse <matt@>
2016-07-11 10:22:19 -0700

    project setup with initial specs and methods for lists and tasks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,10 +1,17 @@
 require('rspec')
 require('task')
+require('pg')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
 
 describe(Task) do
-  before() do
-    Task.clear()
-  end
+
   describe('#description') do
     it('lets you give it a description') do
     test_task = Task.new('scrub the zebra')
@@ -20,7 +27,7 @@ describe(Task) do
 
   describe('#save') do
     it('adds a task to the array of saved tasks') do
-      test_task = Task.new('wash the lion')
+      test_task = Task.new({:description => "learn SQL"})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
@@ -31,6 +38,14 @@ describe(Task) do
       Task.new('wash the lion').save()
       Task.clear()
       expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same task if it has the same description and list ID') do
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(task1).to(eq(task2))
     end
   end
 end
